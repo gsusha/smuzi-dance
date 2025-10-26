553e322976fde04da9f921bca4655ab07bf0a2f1
gsusha <x.dyachenko@gmail.com>
2025-10-26 13:05:37 +0300

    feat: programs (timeline)

--- components/sections/Programs.vue
+++ components/sections/Programs.vue
@@ -11,6 +11,16 @@ import { HorizontalPosition } from '~/types'
       :title-position="HorizontalPosition.Left"
       :subtitle-position="HorizontalPosition.Right"
     />
+    <div class="programs__timeline">
+      <div v-for="point in programs.timeline" :key="point.id" class="programs__timeline-point">
+        <div class="programs__timeline-circle" />
+        <div class="programs__timeline-content">
+          <div class="programs__timeline-age">{{ point.age }}</div>
+          <div class="programs__timeline-title">{{ point.title }}</div>
+          <div class="programs__timeline-description">{{ point.description }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,5 +29,63 @@ import { HorizontalPosition } from '~/types'
   display: flex;
   flex-direction: column;
   gap: 130px;
+
+  &__timeline {
+    display: flex;
+    flex-direction: column;
+
+    &-point {
+      display: flex;
+      gap: 60px;
+      position: relative;
+      padding-bottom: 100px;
+
+      &::before {
+        content: '';
+        position: absolute;
+        left: 10px;
+        top: 36px;
+        bottom: 16px;
+        display: block;
+        width: 1px;
+        background: $border-color;
+      }
+
+      &:last-child {
+        padding: 0;
+
+        &::before {
+          display: none;
+        }
+      }
+    }
+
+    &-circle {
+      width: 21px;
+      height: 21px;
+      border-radius: 50%;
+      background: $accent;
+    }
+
+    &-content {
+      display: flex;
+      flex-direction: column;
+      gap: 14px;
+    }
+
+    &-age {
+      color: $accent;
+      font-family: $font-family-secondary;
+    }
+
+    &-title {
+      font-size: 20px;
+      font-family: $font-family-secondary;
+    }
+
+    &-description {
+      font-size: 14px;
+    }
+  }
 }
 </style>
